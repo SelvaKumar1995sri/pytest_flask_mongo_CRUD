2bf7a91d360b40463d953f335a37245ead020206
selva kumar <selvakumar1995sri@gmail.com>
2022-08-04 22:58:40 +0530

    update commit

--- test/test_skating_app.py
+++ test/test_skating_app.py
@@ -21,7 +21,23 @@ def test_add():
             ),
         content_type='application/json',
     )
+    assert response.status_code == 200
+    
 
-
+def test_update():        
+    response = skating_app.app.test_client().put('/update')
+    assert response.status_code == 200
+    
+def test_delete():        
+    response = skating_app.app.test_client().delete('/delete',
+        data=json.dumps(
+            {
+            "DOB": "03/12/1995",
+            "Name": "legend",
+            "skating Type": "inline"
+            }
+            ),
+        content_type='application/json',
+    )
     assert response.status_code == 200
     
